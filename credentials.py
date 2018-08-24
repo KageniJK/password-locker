526129034c45353057533911bf30065e546daa4c
James Kageni <james.kageni@gmail.com>
2018-08-24 11:02:00 +0300

    populated the credentials class with the properties

---
+++ credentials.py
@@ -0,0 +1,12 @@
+class Credentials:
+    """
+    Class that generates new instances of passwords
+    """
+
+    def __init__(self, account, password):
+        """
+        defining the properties of the class Credentials
+        """
+
+        self.account = account
+        self.password = password
