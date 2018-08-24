926970979ce30b98e5e2045944be5ce005698a91
James Kageni <james.kageni@gmail.com>
2018-08-24 11:12:40 +0300

    populated the user class with the properties

---
+++ user.py
@@ -0,0 +1,10 @@
+class User:
+    """
+    A class to create all instances of the users
+    """
+
+    def __init__(self, name):
+        """
+        defining the properties of the user class
+        """
+        self.name = name
